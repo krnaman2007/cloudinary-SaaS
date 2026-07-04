12e0c0750e28c3c8da5abbe77196d32a89996a2c
Naman Kumar <krnaman.2007@gmail.com>
2026-07-04 10:07:28 +0530

    added video upload functions

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -1,6 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 function VideoUpload() {
+
+  const [file,setFile]=useState<File | null>(null)
+  const [title, setTitle]=useState("")
+  const [description, setDescription]=useState("")
+  const [isUploading, setIsUploading]=useState(false)
+
+  const router=useRouter()
+
+  const MAX_FILE_SIZE=70*1024*1024
+
+  const handleSubmit=async(e: React.FormEvent)=>{
+    e.preventDefault()
+    if(!file) return
+
+    if(file.size>MAX_FILE_SIZE){
+      //TODO: add notification
+      alert("file size too large")
+      return
+    }
+
+    setIsUploading(true)
+    const formData=new FormData()
+    formData.append("file",file)
+    formData.append("title",title)
+    formData.append("description",description)
+    formData.append("originalSize",file.size.toString())
+
+    try {
+      const response=await axios.post("/api/video-upload",formData)
+      //check for 200 response
+    } catch (error) {
+      console.log(error)
+    } finally{
+      setIsUploading(false)
+    }
+  }
+
   return (
     <div>VideoUpload</div>
   )
