358c912f7e2a882afc6ddd50390feab42d7a1a4a
Naman Kumar <krnaman.2007@gmail.com>
2026-07-05 14:19:40 +0530

    created video interface type

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
+import axios from 'axios'
+import VideoCard from '@/components/VideoCard'
 
 function Home() {
+
+  const [videos, setVideos]=useState([])
+  const [loading, setLoading]=useState(true)
+  const [error, setError]=useState(null)
+
+  const fetchVideos=useCallback(async ()=>{
+    try {
+      const response=await axios.get("/api/videos")
+      if(Array.isArray(response.data)){
+        setVideos(response.data)
+      }
+
+    } catch (error) {
+      
+    }
+  },[])
+
   return (
     <div>Home</div>
   )
